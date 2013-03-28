2a059742082bdaf8eb9b49dab30df8dba9c2b368
Dirk Sierd de Vries <dirksierd@me.com>
2013-03-28 14:48:51 +0100

    tests and some more changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   attr_accessible :body, :title
   validates_presence_of :title
   has_many :comments, dependent: :destroy
+
+  def punchline
+    "#{title} heeft #{comments.count} reactie"
+  end
 end
--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "expect a new record" do
+    post = Post.new(title: 'Dit is een nieuw artikel')
+    assert_equal('Dit is een nieuw artikel', post.title)
+  end
+
+  test "my punchlines!" do
+    post = Post.create(title: 'Dit is een artikel')
+    post.comments.create(body: 'Dit is een reactie')
+    assert_equal('Dit is een artikel heeft 1 reactie', post.punchline)
+
+    post.title = 'Dit is een nieuw artikel'
+    assert_equal('Dit is een nieuw artikel heeft 1 reactie', post.punchline)
+  end
 end
